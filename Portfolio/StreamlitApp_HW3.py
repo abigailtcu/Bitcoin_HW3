9d387d6119131fd30e0ae6ac614ca7951976a33a
abigailtcu <abigail.owens@tcu.edu>
2026-04-25 17:27:44 -0500

    Refactor imports and update model configuration

--- Portfolio/StreamlitApp_HW3.py
+++ Portfolio/StreamlitApp_HW3.py
@@ -68,8 +68,8 @@ sm_session = sagemaker.Session(boto_session=session)
 
 MODEL_INFO = {
     "endpoint"  : aws_endpoint,
-    "explainer" : "explainer_fraud.shap",
-    "pipeline"  : "fine_tuned_pipeline.tar.gz",
+    "explainer" : "explainer_project.joblib",
+    "pipeline"  : "finalized_loan_model.tar.gz",
     "keys"      : ['num_int_rate','num_installment','num_funded_amt'],
     "inputs"    : [{"name": k, "type": "number", "min": -1.0, "max": 1.0, "default": 0.0, "step": 0.01} for k in ['num_int_rate','num_installment','num_funded_amt']]
 }
